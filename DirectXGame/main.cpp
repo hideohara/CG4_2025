6ec5db833251a3c6e01a3101e01cb3327b1114ae
ohara <ohara@g.neec.ac.jp>
2025-03-29 11:04:34 +0900

    GameScene処理

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 
 #include "KamataEngine.h"
+#include "GameScene.h"
 using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -12,6 +13,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// DirectXCommon インスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();
 
 	// メインループ
 	while (true) {
@@ -20,12 +25,23 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 
+		// ゲームシーンの更新
+		gameScene->Update();
+
 		// 描画開始
 		dxCommon->PreDraw();
 		
+		// ゲームシーンの描画
+		gameScene->Draw();
+
 		// 描画終了
 		dxCommon->PostDraw();
 	}
+
+	// ゲームシーンの削除
+	delete gameScene;
+	// nullptrの代入
+	gameScene = nullptr;
 
 	// エンジンの終了処理
 	KamataEngine::Finalize();
